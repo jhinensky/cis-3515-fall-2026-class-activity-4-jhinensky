9332c2ad689465a80caf7469c4aeb2a0c5f6435a
jhinensky <177571405+jhinensky@users.noreply.github.com>
2026-09-10 12:23:36 -0400

    eh

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -13,14 +13,29 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        //val numberArray = Array Of Numbers
+        val numberArray = IntArray(20) {(it + 1) * 5}
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        spinner.adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item,
+            numberArray.asList())
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // TODO("Not yet implemented")
+                displayTextView.textSize = parent?.getItemAtPosition(position).toString().toFloat()
+            }
 
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // TODO("Not yet implemented")
+            }
+        }
     }
 }
